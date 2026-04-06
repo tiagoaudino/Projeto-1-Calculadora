b884b1a8154735e8e8684a20804dece476f49e22
Tiago Audino <tiagoaudino711@gmail.com>
2026-04-06 16:05:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function insertToDisplay(data){
+    document.querySelector('#display').value+=data
+
+}
+function clean(){
+    document.querySelector('#display').value=''
+}
+function back(){
+    const display=document.querySelector('#display')
+    display.value=display.value.slice(0,-1)
+
+}
+
+function result(){
+    const display=document.querySelector('#display')
+    try{
+         display.value=eval(display.value)
+
+    } catch{
+        display.value= 'Erro'
+        
+    }
+}
+
+
+
+
+
+
